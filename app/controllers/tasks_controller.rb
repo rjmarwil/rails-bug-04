fbd456eb1f6113c7d4fa63c36ac2c063902f3604
Mark Goldfarb <markgoldfarb@Marks-MacBook-Air.local>
2015-03-06 09:19:30 -0700

    modify create method to find tasklist variable

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,13 +6,19 @@ class TasksController < ApplicationController
   end
 
   def create
-    task_params = params.require(:task).permit(:description, :due_date).merge(task_list_id: params[:task_list_id])
     @task = Task.new(task_params)
+    @task_list = TaskList.find(params[:task_list_id])
     if @task.save
       redirect_to root_path, notice: "Task was created successfully!"
     else
       render :new
     end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description, :due_date).merge(task_list_id: params[:task_list_id])
   end
 
 end
